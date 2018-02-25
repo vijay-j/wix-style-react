89061bf69f4c7d450e19b4dd12b695f15c9262c9
Lior Belinsky <liorbe@LiorBe-MBP15-2.local>
2018-02-25 20:43:51 +0200

    improve story

--- stories/Badge.story.js
+++ stories/Badge.story.js
@@ -1,22 +1,22 @@
-import React from 'react';
+// import React from 'react';
 
-import Text from 'wix-style-react/Text';
-import Badge from 'wix-style-react/Badge';
+// import Text from 'wix-style-react/Text';
+// import Badge from 'wix-style-react/Badge';
 
-export default {
-  category: '12. Other',
-  storyName: '12.1 Badge',
-  componentPath: '../src/Badge',
-  component: Badge,
-  componentProps: {
-    children: 'I\'m a Badge!',
-    dataHook: 'storybook-badge'
-  },
-  examples: (
-    <div>
-      <Text appearance="H0">I fly with alignment <Badge alignment="top">top</Badge></Text>
-      <Text appearance="H0">I squeeze to <Badge>middle</Badge> by default</Text>
-      <Text appearance="H0">I go down with alignment <Badge alignment="bottom">bottom</Badge></Text>
-    </div>
-  )
-};
+// export default {
+//   category: '12. Other',
+//   storyName: '12.1 Badge',
+//   componentPath: '../src/Badge',
+//   component: Badge,
+//   componentProps: {
+//     children: 'I\'m a Badge!',
+//     dataHook: 'storybook-badge'
+//   },
+//   examples: (
+//     <div>
+//       <Text appearance="H0">I fly with alignment <Badge alignment="top">top</Badge></Text>
+//       <Text appearance="H0">I squeeze to <Badge>middle</Badge> by default</Text>
+//       <Text appearance="H0">I go down with alignment <Badge alignment="bottom">bottom</Badge></Text>
+//     </div>
+//   )
+// };
--- stories/Badge/index.js
+++ stories/Badge/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
-import {Badge} from 'wix-ui-backoffice/Badge';
+import Badge from 'wix-style-react/Badge';
 import {Facebook} from 'wix-style-react/Icons';
 
 class ControlledBadgeExample extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {skin: 'default', type: 'outlined'};
+    this.state = {skin: 'default', type: 'solid'};
   }
   render() {
     return(
       <div>
         skin: <input value={this.state.skin} onChange={e => this.setState({skin: e.target.value})}/><br/><br/>
         type: <input value={this.state.type} onChange={e => this.setState({type: e.target.value})}/><br/><br/>
-        <Badge skin={this.state.skin} type={this.state.type} suffixIcon={<Facebook/>} data-hook="storybook-badge">
+        <Badge skin={this.state.skin} type={this.state.type} prefixIcon={<Facebook/>} data-hook="storybook-badge">
           I'M A BADGE!
         </Badge>
       </div>
