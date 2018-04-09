ab6aacee7a5d9d1c1bb36ee90b9a0a5bd314f698
Eran Shabi <eran514@gmail.com>
2018-04-09 17:10:30 +0300

    add getInnerContent method to ModalDriver

--- src/Modal/Modal.driver.js
+++ src/Modal/Modal.driver.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactModal from 'react-modal';
 import ReactTestUtils from 'react-dom/test-utils';
 
 const modalDriverFactory = ({element, wrapper, component}) => {
@@ -15,6 +16,7 @@ const modalDriverFactory = ({element, wrapper, component}) => {
     isThemeExist: theme => !!getPortal().querySelector(`.${theme}`),
     getChildBySelector: selector => getPortal().querySelector(selector),
     isScrollable: () => !getPortal().classList.contains('portalNonScrollable'),
+    getInnerContent: () => wrapper.find(ReactModal).node.portal,
     clickOnOverlay: () => {
       const overlay = getOverlay();
       ReactTestUtils.Simulate.click(overlay);
